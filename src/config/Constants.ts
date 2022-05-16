1e350cff8da1cf0cb596ea50c35beffa89de6728
devjoaovictor <devjoaovictor1@gmail.com>
2022-05-15 21:37:14 -0300

    primeiro commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://devjoao:<password>@encurtador-url-dio.x0iem.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
